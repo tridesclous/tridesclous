3ad65c58a13a9d409a0103f203d799b1ffa6f6fa
Samuel Garcia <sgarcia@sgarcia-DellXPS13>
2015-12-01 14:48:24 +0100

    Start datamanager.

---
+++ tridesclous/datamanager.py
@@ -0,0 +1,182 @@
+import os
+import pandas as pd
+import numpy as np
+import json
+
+
+class DataManager:
+    """
+    This class handle data for each step of spike sorting: raw data, filtered data, peaks position,
+    waveform, ....
+    
+    All data are copy in a directory in several hdf5 (pandas+pytables) files so a spike sorting
+    can be continued/verifiyed later on without need to recompute all steps.
+    
+    Internally the hdf5 (pytables formated) contains:
+    'info' : a Series that contains sampling_rate, nb_channels, ...
+    ''segment_0/unfiltered_signals' : non filetred signals of segment 0
+    ''segment_0/signals' : filetred signals of segment 0
+    
+    
+    """
+    def __init__(self, dirname = 'test', complib = 'blosc', complevel= 9):
+        self.dirname = dirname
+        
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        
+        self.data_filename = os.path.join(self.dirname, 'data.h5')
+        if not os.path.exists(self.data_filename):
+            #TODO some initilisation_list
+            pass
+        
+        self.store = pd.HDFStore(self.data_filename, complib = complib, complevel = complevel,  mode = 'a')
+        
+        if 'info' in self.store:
+            self.info = self.store['info']
+        else:
+            self.info = None
+        
+        if 'segments' in self.store:
+            self.segments = self.store['segments']
+        else:
+            self.segments = None
+            
+        
+    
+    def __repr__(self):
+        t = """DataManager <{}>
+Workdir: {}""".format(id(self), self.data_filename)
+        if self.info is None:
+            t  += "\nNot initialized"
+            return t
+        t += """
+sampling_rate: {}
+nb_channel: {}
+nb_segments: {}""".format(self.sampling_rate, self.nb_channel, self.nb_segments)
+        return t
+    
+    @property
+    def sampling_rate(self):
+        if self.info is not None:
+            return float(self.info['sampling_rate'])
+
+    @property
+    def nb_channel(self):
+        if self.info is not None:
+            return int(self.info['nb_channel'])
+
+    @property
+    def nb_segments(self):
+        if self.segments is not None:
+            return len(self.segments)
+    
+    
+    def initialize(self, sampling_rate = None, nb_channel = None):
+        """
+        Initialise the Datamanager.
+        
+        Arguments
+        -----------------
+        sampling_rate: float
+            The sampling rate in Hz
+        nb_channel: int
+            nb of channel
+        """
+        assert sampling_rate is not None, 'You must provide a sampling rate'
+        
+        if self.info is None:
+            self.info = pd.Series()
+        self.info['sampling_rate'] = sampling_rate
+        self.info['nb_channel'] = nb_channel
+        
+        if self.segments is None:
+            self.segments = pd.DataFrame(columns = ['t_start', 't_stop'], dtype = 'float64')
+        
+        self.flush_info()
+
+    def flush_info(self):
+        self.store['info'] = self.info
+        self.store['segments'] = self.segments
+        self.store.flush()
+        
+    def append_signals(self, signals, seg_num=0, sampling_rate = None, t_start = 0., already_hp_filtered = False, ):
+        """
+        Append signals segment in the store.
+        If the segment do not exist it is created in the store.
+        Else the signals chunk is append to the previsous one.
+        
+        Arguments
+        -----------------
+        signals: np.ndarray
+            Signals 2D array shape (nb_sampleXnb_channel).
+        seg_num: int
+            The segment num.
+        t_start: float 
+            Time stamp of the first sample.
+        
+        """
+        if signals.ndim==1:
+            signals = signals[:, None]
+        
+        if self.info is None:
+            self.initialize(sampling_rate = sampling_rate, nb_channel = signals.shape[1])
+
+        assert signals.shape[1]==self.info['nb_channel'], 'Wrong shape {} ({} chans)'.format(signals.shape, self.info['nb_channel'])
+        assert sampling_rate == self.info['sampling_rate'], 'Wrong sampling_rate {} {}'.format(sampling_rate, self.info['sampling_rate'])
+        
+        times = np.arange(signals.shape[0], dtype = 'float64')/self.sampling_rate + t_start
+        df = pd.DataFrame(signals, index = times)
+        
+        path = 'segment_{}'.format(seg_num)
+        if already_hp_filtered:
+            path += '/signals'
+        else:
+            path += '/unfiltered_signals'
+        
+        if seg_num in self.segments.index:
+            #this check overlap if trying to write an already exisiting chunk
+            # theorically this should work but the index will unefficient when self.store.select
+            assert np.all(~((times>=self.segments.loc[seg_num, 't_start']) & (times<=self.segments.loc[seg_num, 't_stop']))), 'data already in store for seg_num {}'.format(seg_num)
+            
+        df.to_hdf(self.store, path, format = 'table', append=True)
+        
+        if seg_num in self.segments.index:
+            self.segments.loc[seg_num, 't_start'] = min(self.segments.loc[seg_num, 't_start'], times[0])
+            self.segments.loc[seg_num, 't_stop'] =  max(times[-1], self.segments.loc[seg_num, 't_stop'])
+        else:
+            self.segments.loc[seg_num, 't_start'] = times[0]
+            self.segments.loc[seg_num, 't_stop'] = times[-1]
+        self.flush_info()
+        
+        self.store.create_table_index(path, optlevel=9, kind='full')
+
+    def get_signals(self, seg_num=0, t_start = None, t_stop = None, filtered = True):
+        """
+        Get a chunk of signals in the dataset.
+        This internally use self.store.select from pandas.
+        This should be efficient... and avoid loading everything in memory.
+        
+        Arguments
+        -----------------
+        seg_num: int
+        
+        """
+        path = 'segment_{}'.format(seg_num)
+        if filtered:
+            path += '/signals'
+        else:
+            path += '/unfiltered_signals'
+        
+        if t_start is None and t_stop is None:
+            query = None
+        elif t_start is not None and t_stop is None:
+            query = 'index>=t_start'
+        elif t_start is None and t_stop is not None:
+            query = 'index<t_stop'
+        elif t_start is not None and t_stop is not None:
+            query = 'index>=t_start & index<t_stop'
+        
+        return self.store.select(path, query)
+
+    
---
+++ tridesclous/tests/test_datamanager.py
@@ -0,0 +1,59 @@
+import pytest
+import h5py
+import os, tempfile
+import numpy as np
+
+
+from tridesclous import DataManager
+from urllib.request import urlretrieve
+
+def download_locust():
+    name = 'locust20010201.hdf5'
+    distantfile = 'https://zenodo.org/record/21589/files/'+name
+    localdir = os.path.dirname(os.path.abspath(__file__))
+    
+    if not os.access(localdir, os.W_OK):
+        localdir = tempfile.gettempdir()
+    localfile = os.path.join(os.path.dirname(__file__), name)
+    
+    if not os.path.exists(localfile):
+        urlretrieve(distantfile, localfile)
+    hdf = h5py.File(localfile,'r')
+    ch_names = ['ch09','ch11','ch13','ch16']
+    sigs = np.array([hdf['Continuous_1']['trial_01'][name][...] for name in ch_names]).transpose()
+    sigs = (sigs.astype('float32') - 2**15.) / 2**15
+    
+    return sigs
+
+
+
+def test_spikesorter():
+    if os.path.exists('test/data.h5'):
+        os.remove('test/data.h5')
+    data = DataManager(dirname = 'test')
+    print(data)
+    #data from locust
+    signals = download_locust()
+    
+    for seg_num in range(3):
+        data.append_signals(signals, seg_num = seg_num,t_start = 0.+5*seg_num, sampling_rate =  15000., already_hp_filtered = True)
+    
+    print(data)
+    print(data.segments)
+    print(data.store)
+    
+    assert data.get_signals(seg_num=0).shape == (431548, 4)
+    assert data.get_signals(seg_num=0, t_start=3.).shape==(386548, 4)
+    assert data.get_signals(seg_num=0, t_stop=5.).shape == (75000, 4)
+    assert data.get_signals(seg_num=0, t_start=3., t_stop = 5.).shape == (30000, 4)
+    
+    
+    
+    
+    
+    
+if __name__=='__main__':
+    test_spikesorter()
+    
+    
+    
