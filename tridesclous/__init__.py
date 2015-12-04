685e3af82a99f37c4158e5a3dee009c6f48ce7c5
Samuel Garcia <sgarcia@sgarcia-DellXPS13>
2015-12-04 16:24:11 +0100

    STart GUI.

--- tridesclous/__init__.py
+++ tridesclous/__init__.py
@@ -6,3 +6,10 @@ from .waveformextractor import *
 from .clustering import Clustering
 from .mpl_plot import *
 
+try:
+    from .gui import *
+except ImportError:
+    import logging
+    logging.warning('INteractive GUI not availble')
+    pass
+
---
+++ tridesclous/gui/tests/test_gui.py
@@ -0,0 +1,11 @@
+from tridesclous import *
+
+
+def test_gui():
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    test_gui()
