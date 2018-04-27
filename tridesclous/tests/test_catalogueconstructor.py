a8be8812282d2990c31819b5762ee39ce60c9de4
Samuel Garcia <sam.garcia.die@gmail.com>
2018-04-27 14:19:26 +0200

    Speedup make catalogue.

--- tridesclous/tests/test_catalogueconstructor.py
+++ tridesclous/tests/test_catalogueconstructor.py
@@ -8,7 +8,7 @@ from tridesclous.dataio import DataIO
 from tridesclous.catalogueconstructor import CatalogueConstructor
 from tridesclous.tools import median_mad
 
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
 def test_catalogue_constructor():
     if os.path.exists('test_catalogueconstructor'):
@@ -140,18 +140,18 @@ def test_catalogue_constructor():
         #~ catalogueconstructor.compute_centroid()
         #~ similarity, cluster_labels = catalogueconstructor.compute_similarity()
         #~ print(cluster_labels)
-        #~ fig, ax = pyplot.subplots()
+        #~ fig, ax = plt.subplots()
         #~ ax.matshow(similarity)
-        #~ pyplot.show()
+        #~ plt.show()
         
         #plot
         #~ wf = catalogueconstructor.some_waveforms
         #~ wf = wf.swapaxes(1,2).reshape(wf.shape[0], -1)
         
-        #~ fig, ax = pyplot.subplots()
+        #~ fig, ax = plt.subplots()
         #~ ax.plot(np.median(wf, axis=0), color='b')
         
-        #~ pyplot.show()
+        #~ plt.show()
 
 #~ def show_similarity():
     
@@ -192,7 +192,7 @@ def compare_nb_waveforms():
     
     print(catalogueconstructor)
     
-    fig, axs = pyplot.subplots(nrows=2)
+    fig, axs = plt.subplots(nrows=2)
     
     colors = ['r', 'g', 'b']
     for i, nb_max in enumerate([100, 1000, 10000]):
@@ -210,69 +210,26 @@ def compare_nb_waveforms():
     axs[0].legend()
     axs[0].set_title('median')
     axs[1].set_title('mean')
-    pyplot.show()        
+    plt.show()        
     
 
 def test_make_catalogue():
-    if os.path.exists('test_catalogueconstructor'):
-        shutil.rmtree('test_catalogueconstructor')
-        
     dataio = DataIO(dirname='test_catalogueconstructor')
-    localdir, filenames, params = download_dataset(name='olfactory_bulb')
-    dataio.set_data_source(type='RawData', filenames=filenames, **params)
-    #~ dataio.add_one_channel_group(channels=range(14), chan_grp=0)
-    dataio.add_one_channel_group(channels=[5, 6, 7, 8, 9], chan_grp=0)
-    
 
-    catalogueconstructor = CatalogueConstructor(dataio=dataio)
+    cc = CatalogueConstructor(dataio=dataio)
 
-    catalogueconstructor.set_preprocessor_params(chunksize=1024,
-            
-                                    #signal preprocessor
-                                    highpass_freq=300.,
-                                    lowpass_freq=5000.,
-                                    lostfront_chunksize=128,
-                                    
-                                    #peak detector
-                                    peak_sign='-', relative_threshold=7, peak_span=0.0005,
-                                    )
+    #~ cc.make_catalogue()
+    cc.make_catalogue_for_peeler()
     
-    t1 = time.perf_counter()
-    catalogueconstructor.estimate_signals_noise(seg_num=0, duration=10.)
-    t2 = time.perf_counter()
-    print('estimate_signals_noise', t2-t1)
+    #~ for i, k in cc.catalogue['label_to_index'].items():
     
-    t1 = time.perf_counter()
-    catalogueconstructor.run_signalprocessor()
-    t2 = time.perf_counter()
-    print('run_signalprocessor', t2-t1)
-
-    print(catalogueconstructor)
+        #~ fig, ax = plt.subplots()
+        #~ ax.plot(cc.catalogue['centers0'][i, :, :].T.flatten(), color='b')
+        #~ ax.plot(cc.catalogue['centers1'][i, :, :].T.flatten(), color='g')
+        #~ ax.plot(cc.catalogue['centers2'][i, :, :].T.flatten(), color='r')
+        
     
-    
-    
-    t1 = time.perf_counter()
-    catalogueconstructor.extract_some_waveforms(n_left=-12, n_right=15,  nb_max=10000)
-    t2 = time.perf_counter()
-    print('extract_some_waveforms', t2-t1)
-
-
-    # PCA
-    t1 = time.perf_counter()
-    catalogueconstructor.project(method='pca', n_components=12, batch_size=16384)
-    t2 = time.perf_counter()
-    print('project', t2-t1)
-    
-    # cluster
-    t1 = time.perf_counter()
-    catalogueconstructor.find_clusters(method='kmeans', n_clusters=13)
-    t2 = time.perf_counter()
-    print('find_clusters', t2-t1)
-    
-    # trash_small_cluster
-    catalogueconstructor.trash_small_cluster()
-    
-    catalogueconstructor.make_catalogue()
+    #~ plt.show()
     
 
 
@@ -293,11 +250,11 @@ def test_create_savepoint_catalogue_constructor():
 
     
 if __name__ == '__main__':
-    test_catalogue_constructor()
+    #~ test_catalogue_constructor()
     
     #~ compare_nb_waveforms()
     
-    #~ test_make_catalogue()
+    test_make_catalogue()
     #~ test_ratio_amplitude()
     
     #~ test_create_savepoint_catalogue_constructor()
