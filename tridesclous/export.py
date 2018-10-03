a398dde981de8a21ce4cbf2df7db1f01beade627
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2018-10-03 16:03:26 -0400

    Make CsvSpikeExporter use comma delimiter instead of tab
    Also fix spelling of CsvSpikeExporter

--- tridesclous/export.py
+++ tridesclous/export.py
@@ -13,7 +13,7 @@ except ImportError:
 
 class GenericSpikeExporter:
     def __call__(self,spikes, catalogue, seg_num, chan_grp, export_path,
-                        split_by_cluster=False, 
+                        split_by_cluster=False,
                         use_cell_label=True,
                         #~ use_index=True,
                         ):
@@ -22,17 +22,17 @@ class GenericSpikeExporter:
         #~ print('export', spikes.size, seg_num, export_path)
         #~ print('split_by_cluster', split_by_cluster, 'use_cell_label', use_cell_label)
         clusters = catalogue['clusters']
-        
+
         spike_labels = spikes['cluster_label']
         if use_cell_label:
             spike_labels = spikes['cluster_label'].copy()
             for l in clusters:
                 mask = spike_labels==l['cluster_label']
                 spike_labels[mask] = l['cell_label']
-        
+
         spike_indexes = spikes['index']
-        
-        
+
+
         out_data = OrderedDict()
         if split_by_cluster:
             if use_cell_label:
@@ -46,12 +46,12 @@ class GenericSpikeExporter:
                 out_data[label_name + '#'+ str(k)] = (spike_indexes[keep], spike_labels[keep])
         else:
             out_data['cell#all'] = (spike_indexes, spike_labels)
-        
+
         name = 'spikes - segNum {} - chanGrp {}'.format(seg_num, chan_grp)
         filename = os.path.join(export_path, name)
         self.write_out_data(out_data, filename)
 
-class CvsSpikeExporter(GenericSpikeExporter):
+class CsvSpikeExporter(GenericSpikeExporter):
     ext = 'csv'
     def write_out_data(self, out_data, filename):
         for key, (spike_indexes, spike_labels) in out_data.items():
@@ -61,11 +61,11 @@ class CvsSpikeExporter(GenericSpikeExporter):
     def _write_one_file(self, filename, labels, indexes):
         rows = [''] * len(labels)
         for i in range(len(labels)):
-            rows[i]='{}\t{}\n'.format(labels[i], indexes[i])
+            rows[i]='{},{}\n'.format(labels[i], indexes[i])
         with open(filename, 'w') as out:
             out.writelines(rows)
 
-export_csv = CvsSpikeExporter()
+export_csv = CsvSpikeExporter()
 
 
 class MatlabSpikeExporter(GenericSpikeExporter):
@@ -73,7 +73,7 @@ class MatlabSpikeExporter(GenericSpikeExporter):
 
     def write_out_data(self, out_data, filename):
         mdict = {}
-        
+
         for key, (spike_indexes, spike_labels) in out_data.items():
             mdict['index_'+key] = spike_indexes
             mdict['label_'+key] =spike_labels
