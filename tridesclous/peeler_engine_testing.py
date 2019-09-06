d17da44f155a7f50430c265f0b439df73badd68c
Samuel Garcia <sam.garcia.die@gmail.com>
2019-09-06 23:07:17 +0200

    Try new criteria for accept template.

--- tridesclous/peeler_engine_testing.py
+++ tridesclous/peeler_engine_testing.py
@@ -23,6 +23,7 @@ import matplotlib.pyplot as plt
 
 class PeelerEngineTesting(PeelerEngineClassic):
     #~ pass
+
     #~ def estimate_jitter(self, left_ind, cluster_idx):
         #~ return 0
     
@@ -46,17 +47,29 @@ class PeelerEngineTesting(PeelerEngineClassic):
         wf_nrj = np.sum(full_wf**2, axis=0)
         
         
-        residual_nrj = np.sum((full_wf-pred_wf)**2, axis=0)
+        
         
         # criteria per channel
-        label = self.catalogue['cluster_labels'][cluster_idx]
-        weight = self.weight_per_template[label]
-        crietria_weighted = (wf_nrj>residual_nrj).astype('float') * weight
-        accept_template = np.sum(crietria_weighted) >= 0.9 * np.sum(weight)
+        #~ residual_nrj = np.sum((full_wf-pred_wf)**2, axis=0)
+        #~ label = self.catalogue['cluster_labels'][cluster_idx]
+        #~ weight = self.weight_per_template[label]
+        #~ crietria_weighted = (wf_nrj>residual_nrj).astype('float') * weight
+        #~ accept_template = np.sum(crietria_weighted) >= 0.9 * np.sum(weight)
+        
+        weigth = pred_wf ** 2
+        residual = (full_wf-pred_wf)
+        s = np.sum((full_wf**2>residual**2).astype(float) * weigth)
+        #~ s = np.sum((pred_wf**2*weigth)>(residual*weigth))
+        accept_template = s >np.sum(weigth) * 0.8
+        #~ print(s, np.sum(weigth) , np.sum(weigth)  * 0.7)
+        #~ exit()
+        
         
         #DEBUG
-        #~ label = self.catalogue['cluster_labels'][cluster_idx]
-        #~ if label in (5, ):
+        label = self.catalogue['cluster_labels'][cluster_idx]
+        #~ if label in (0, ):
+        #~ if False:
+        if True:
             
             #~ print('accept_tempate',accept_template, 'label', label)
             #~ print(wf_nrj>residual_nrj)
@@ -70,19 +83,28 @@ class PeelerEngineTesting(PeelerEngineClassic):
                 #~ print(weight)
                 #~ print(crietria_weighted)
                 #~ print()
-                
+            print(s, np.sum(weigth) , np.sum(weigth)  * 0.8)
             
-            #~ fig, ax = plt.subplots()
-            #~ ax.plot(full_wf.T.flatten(), color='b')
-            #~ if accept_template:
-                #~ ax.plot(pred_wf.T.flatten(), color='g')
-            #~ else:
-                #~ ax.plot(pred_wf.T.flatten(), color='r')
+            fig, axs = plt.subplots(nrows=3, sharex=True)
+            axs[0].plot(full_wf.T.flatten(), color='b')
+            if accept_template:
+                axs[0].plot(pred_wf.T.flatten(), color='g')
+            else:
+                axs[0].plot(pred_wf.T.flatten(), color='r')
             
-            #~ ax.plot((full_wf-pred_wf).T.flatten(), color='m')
+            axs[0].plot((full_wf-pred_wf).T.flatten(), color='m')
+            
+            axs[1].plot((full_wf**2).T.flatten(), color='b')
+            axs[1].plot((residual**2).T.flatten(), color='m')
+            
+            criterium = (full_wf**2>residual**2).astype(float) * weigth
+            axs[2].plot(criterium.T.flatten(), color='k')
             
             
-            #~ plt.show()
+            
+            
+            
+            plt.show()
             
         
         #~ #ENDDEBUG
