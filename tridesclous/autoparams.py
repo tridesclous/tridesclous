b90ad234f851f0c9e49a8440602b98f58edec975
Samuel Garcia <sam.garcia.die@gmail.com>
2020-04-23 21:27:17 +0200

    force opencl in peeler

--- tridesclous/autoparams.py
+++ tridesclous/autoparams.py
@@ -253,18 +253,18 @@ def get_auto_params_for_peelers(dataio, chan_grp=0):
         params['use_sparse_template'] = True
         params['sparse_threshold_mad'] = 1.5
         
-        #~ if HAVE_PYOPENCL:
+        if HAVE_PYOPENCL:
             #~ params['argmin_method'] = 'opencl'
             #~ params['engine'] = 'geometrical'
-            #~ params['engine'] = 'geometrical_opencl' # Still experimental
-        #~ elif HAVE_NUMBA:
-            #~ params['argmin_method'] = 'numba'
-            #~ params['engine'] = 'geometrical'
-        #~ else:
-            #~ params['argmin_method'] = 'numpy'
-            #~ params['engine'] = 'geometrical'
+            
+            params['argmin_method'] = 'opencl'
+            params['engine'] = 'geometrical_opencl' # Still experimental
+        elif HAVE_NUMBA:
+            params['argmin_method'] = 'numba'
+            params['engine'] = 'geometrical'
+        else:
+            params['argmin_method'] = 'numpy'
+            params['engine'] = 'geometrical'
         
-        params['engine'] = 'geometrical'
-        params['argmin_method'] = 'numba'
         
     return params
